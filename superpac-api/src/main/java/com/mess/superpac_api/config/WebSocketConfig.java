c4613cbeb9e0addd3f84062c6089d6438451c715
Messaoud Rm <messaoud.ramouche@example.com>
2026-08-16 17:54:29 +0200

    for aws 2

--- superpac-api/src/main/java/com/mess/superpac_api/config/WebSocketConfig.java
+++ superpac-api/src/main/java/com/mess/superpac_api/config/WebSocketConfig.java
@@ -24,5 +24,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/game-ws")
                 .setAllowedOriginPatterns(allowedOrigins.split(","));
+    }
 
 }
